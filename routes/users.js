baa2d3fd22500808eb253f0c6c74589c3ad6a8cb
reidvr <reidvr@gmail.com>
2019-04-05 13:46:36 +1300

    Link nav with routes

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,14 @@ const db = require('../db')
 const router = express.Router()
 
 router.get('/', (req, res) => {
- res.render('partials/index');
+ res.render('partials/intro');
 })
 
+router.get('/home', (req, res) => {
+      res.render('partials/index');
+     })
 
-router.get('/tasks/', (req, res) => {
+router.get('/tasks', (req, res) => {
       Promise.all([
              db.GetAssignmentByStatus('Backlog'),
              db.GetAssignmentByStatus('To Do'),
@@ -33,10 +36,60 @@ router.get('/tasks/', (req, res) => {
             //       obj[newKey] = x
             //       console.log(obj)
             // })
-
-            res.render('partials/overview')
+            console.log(kanban);
+            res.render('partials/overview', kanban)
             
       })
+
+// { backlog:
+//    [ { id: 5,
+//        description: 'Git Push',
+//        status: 'Backlog',
+//        user_id: 5,
+//        task_id: 1,
+//        first_name: 'Blaine',
+//        last_name: 'Western',
+//        image: null 
+//       },
+//      { id: 3,
+//        description: 'Styling',
+//        status: 'Backlog',
+//        user_id: 3,
+//        task_id: 5,
+//        first_name: 'Cliff',
+//        last_name: 'Ah-Leong',
+//        image: null } 
+//       ],
+//   todo:
+//    [ { id: 4,
+//        description: 'Site Map',
+//        status: 'To Do',
+//        user_id: 4,
+//        task_id: 2,
+//        first_name: 'Ashleigh',
+//        last_name: 'Vivier',
+//        image: null } ],
+//   done:
+//    [ { id: 2,
+//        description: 'Migrations',
+//        status: 'Done',
+//        user_id: 2,
+//        task_id: 4,
+//        first_name: 'Fud',
+//        last_name: 'Z',
+//        image: null } ],
+//   ongoing:
+//    [ { id: 1,
+//        description: 'Routes',
+//        status: 'Ongoing',
+//        user_id: 1,
+//        task_id: 3,
+//        first_name: 'Vinnie',
+//        last_name: 'Reid',
+//        image: null } ] 
+// }
+
+
 
       // db.GetAssignmentByStatus('To Do')
 
@@ -44,4 +97,16 @@ router.get('/tasks/', (req, res) => {
 
       // db.GetAssignmentByStatus('Ongoing')
 })
+
+router.get('/addUser', (req, res) => {
+      res.render('partials/addUser');
+})
+
+router.get('/addTask', (req, res) => {
+      res.render('partials/addTask');
+})
+router.get('/editTask', (req, res) => {
+      res.render('partials/editTask');
+})
+
 module.exports = router
